59f8978d8f3bc0c119c6f6583d96c4b7a187fd8c
tokuotsu <t.tokuhiro0317@gmail.com>
2022-06-30 21:45:34 +0900

    6th commit

--- cronjob.py
+++ cronjob.py
@@ -6,7 +6,7 @@ scheduler = BlockingScheduler()
 # 9-21時→0-12
 scheduler.add_job(main_former, 'cron', hour="0-14", minute=0)
 scheduler.add_job(main_latter, 'cron', hour="0-14", minute=30)
-scheduler.add_job(main_difference, 'interval', minute=15)
+scheduler.add_job(main_difference, 'interval', minutes=15)
 
 # scheduler.add_job(main_latter, 'cron', minute=30)
 # scheduler.add_job(test, 'cron', hour=10)
