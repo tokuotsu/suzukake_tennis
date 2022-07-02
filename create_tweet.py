0a198838ee6e27441cfd087a79e547dd7f5774c6
tokuotsu <t.tokuhiro0317@gmail.com>
2022-07-02 14:08:58 +0900

    10th_4

--- create_tweet.py
+++ create_tweet.py
@@ -55,7 +55,7 @@ def getJST():
 def make_body_day(search_date, now_date, dictionary, type_season):
     now_date = now_date.strftime("%m/%d %H:%M:%S")
     search_date = search_date.strftime("%m/%d") + f"({num2youbi(search_date.strftime('%w'))})"
-    body = f"{search_date}\n{'='*6} |  A  |  B  |  C  |\n"
+    body = f"{search_date}\n{'='*7} |  A  |  B  |  C  |\n"
     for key, value in dictionary.items():
         # value = np.array(value)
         # 100で割った余りが0なら×、それ以外は全て○
