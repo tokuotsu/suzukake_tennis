2b0f23b7e520460478d5087047b2f3f355f3c66c
tokuotsu <t.tokuhiro0317@gmail.com>
2022-07-02 10:51:26 +0900

    10th commit

--- cronjob.py
+++ cronjob.py
@@ -10,15 +10,15 @@ scheduler.add_job(main_latter, 'cron', hour="10,18", minute="0")
 
 # 更新
 # 下2段は教務webメンテンナンス中
-scheduler.add_job(main_difference_former, 'cron', hour="0, 1, 4", minute="20", day_of_week="mon, wed, thu, sat, sun")
-scheduler.add_job(main_difference_former, 'cron', hour="7-23", minute="20", day_of_week="mon, wed, thu, sat, sun")
-scheduler.add_job(main_difference_former, 'cron', hour="0, 1", minute="20", day_of_week="tue, fri")
-scheduler.add_job(main_difference_former, 'cron', hour="9-23", minute="20", day_of_week="tue, fri")
+scheduler.add_job(main_difference_former, 'cron', hour="0, 1, 4", minute="15", day_of_week="mon, wed, thu, sat, sun")
+scheduler.add_job(main_difference_former, 'cron', hour="7-23", minute="15", day_of_week="mon, wed, thu, sat, sun")
+scheduler.add_job(main_difference_former, 'cron', hour="0, 1", minute="15", day_of_week="tue, fri")
+scheduler.add_job(main_difference_former, 'cron', hour="9-23", minute="15", day_of_week="tue, fri")
 
-scheduler.add_job(main_difference_latter, 'cron', hour="0, 1, 4", minute="40", day_of_week="mon, wed, thu, sat, sun")
-scheduler.add_job(main_difference_latter, 'cron', hour="7-23", minute="40", day_of_week="mon, wed, thu, sat, sun")
-scheduler.add_job(main_difference_latter, 'cron', hour="0, 1", minute="40", day_of_week="tue, fri")
-scheduler.add_job(main_difference_latter, 'cron', hour="9-23", minute="40", day_of_week="tue, fri")
+scheduler.add_job(main_difference_latter, 'cron', hour="0, 1, 4", minute="45", day_of_week="mon, wed, thu, sat, sun")
+scheduler.add_job(main_difference_latter, 'cron', hour="7-23", minute="45", day_of_week="mon, wed, thu, sat, sun")
+scheduler.add_job(main_difference_latter, 'cron', hour="0, 1", minute="45", day_of_week="tue, fri")
+scheduler.add_job(main_difference_latter, 'cron', hour="9-23", minute="45", day_of_week="tue, fri")
 
 # scheduler.add_job(main_latter, 'cron', minute=30)
 # scheduler.add_job(test, 'cron', hour=10)
