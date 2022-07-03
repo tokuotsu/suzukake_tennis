33f2da4ac1d38b5df64ef48244452bc98cf3f637
tokuotsu <t.tokuhiro0317@gmail.com>
2022-07-03 16:50:36 +0900

    11st_3

--- cronjob.py
+++ cronjob.py
@@ -8,7 +8,7 @@ scheduler = BlockingScheduler(timezone="Asia/Tokyo")
 # 定期
 # scheduler.add_job(main_former, 'cron', hour="9,17", minute="0")
 # scheduler.add_job(main_latter, 'cron', hour="10,18", minute="0")
-scheduler.add_job(main, 'cron', hour="7, 17", minute="0")
+scheduler.add_job(main, 'cron', hour="9, 17", minute="0")
 
 # 更新
 # 下2段は教務webメンテンナンス中
