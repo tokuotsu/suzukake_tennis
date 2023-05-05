d96e4e1d39b7d7be4fbf44ea8ceea5dd0b4cfc4c
tokuotsu <t.tokuhiro0317@gmail.com>
2023-05-05 12:03:26 +0900

    月-金の表示変更

--- main.py
+++ main.py
@@ -151,6 +151,7 @@ def scraping(is_former=True, is_difference=False):
             # 土日のB面の行は1、平日は予約できるっぽい
             # if search_date.weekday() >= 5:
             #     df_str.iloc[1,:]="teacher_only"
+            df_str.iloc[1,:]="teacher_only"
         a = np.where(np.array(df_str)=="teacher_only", a+100, a)
         
         new_dict = defaultdict(list)
